8f6b201e8039f05455e79e67d10cff12dbcd1b23
Rab Campbell <56073739+LFCRab@users.noreply.github.com>
2021-05-01 13:58:37 +0100

    :sparkles: going forward slowly :sparkles:

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.app')
 
 @section('content')
-    <h1>Hello Kelly</h1>
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            Post
+        </div>
+    </div>
 @endsection
