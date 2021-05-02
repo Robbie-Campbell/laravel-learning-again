3f2e11174299e3026d3ac13578b40f0413a07f2c
Rab Campbell <56073739+LFCRab@users.noreply.github.com>
2021-05-02 16:18:46 +0100

    :sparkles: done a little bit more :sparkles:

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+
 Route::get('/', function () {
-    return view('main.index');
+    return view('posts.index');
 });
